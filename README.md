c44a726767dea29c07d615845f613e496ee6a51b
Ishizuka427 <56011102+Ishizuka427@users.noreply.github.com>
2023-09-11 23:31:10 +0900

    Convert Markdown to HTML

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 # Markdown_converter
+
 Converter Markdown to HTML in node.js
 
 ## Set up
+
 Create a package.json
+
 ```sh
 $ npm init --yes
 ```
 
 Install commander@9.0
+
 ```sh
 npm install commander@9.0
 ```
 
+Installc marked@4.0
+
+```sh
+npm install marked@4.0
+```
+
 Set the module to type to use ECMAScript with the pkg command
+
 ```sh
 $ npm pkg set type=module
 ```
--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 // commanderモジュールからprogramオブジェクトをインポートする
 import {program} from 'commander';
+// fs/promisesモジュールからreadFile関数を読み込む
+import * as fs from 'node:fs/promises';
+// markedモジュールからmarkedオブジェクトをインポートする
+import {marked} from 'marked';
 
-// コマンドライン引数をcommanderでパースする
+// gfmオプションを定義する
+program.option('--gfm', 'GFMを有効にする');
+// コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
-
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+  gfm: options.gfm ?? false,
+};
+
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, {encoding: 'utf8'})
+  .then((file) => {
+    // MarkdownファイルをHTML文字列に変換する
+    const html = marked.parse(file, {
+      // オプションの値を使用する
+      gfm: cliOptions.gfm,
+    });
+    console.log(html);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+    process.exit(1);
+  });
